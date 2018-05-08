33e61cdbfed61865a52ac6cfc2ba5855b8589089
Luka Kacil <luka.kacil@gmail.com>
2018-05-08 14:01:30 +0200

    Remove empty spaces

--- contracts/mocks/ERC721EnumerableMock.sol
+++ contracts/mocks/ERC721EnumerableMock.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.23;
 import "../tokens/ERC721EnumerableImplementation.sol";
 
 contract ERC721EnumerableMock is ERC721EnumerableImplementation {
- 
+
 	function mint(address _to,
                   uint256 _id)
       external
