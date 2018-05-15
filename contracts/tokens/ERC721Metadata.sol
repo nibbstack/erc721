96b0e39395a81d9a8d5768961b158afef8a566d1
Tadej Vengust <vengust.tadej@gmail.com>
2018-05-15 17:02:40 +0200

    Comment style change.

--- contracts/tokens/ERC721Metadata.sol
+++ contracts/tokens/ERC721Metadata.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.4.23;
 
-/**
+/*
  * @dev Optional metadata extension for ERC-721 non-fungible token standard.
  * See https://goo.gl/pc9yoS.
  */
 interface ERC721Metadata {
 
-  /**
+  /*
    * @dev Returns a descriptive name for a collection of NFTs in this contract.
    */
   function name()
@@ -14,7 +14,7 @@ interface ERC721Metadata {
     view
     returns (string _name);
 
-  /**
+  /*
    * @dev Returns a abbreviated name for a collection of NFTs in this contract.
    */
   function symbol()
@@ -22,7 +22,7 @@ interface ERC721Metadata {
     view
     returns (string _symbol);
 
-  /**
+  /*
    * @dev Returns a distinct Uniform Resource Identifier (URI) for a given asset. It Throws if
    * `_tokenId` is not a valid NFT. URIs are defined in RFC3986. The URI may point to a JSON file
    * that conforms to the "ERC721 Metadata JSON Schema".
@@ -31,5 +31,4 @@ interface ERC721Metadata {
     external
     view
     returns (string);
-
 }
