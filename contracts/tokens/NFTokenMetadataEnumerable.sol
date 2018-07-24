f1f9e4687b9e2ca4775d67d4100653cd8ff6f711
Tadej Vengust <vengust.tadej@gmail.com>
2018-07-24 08:28:16 +0200

    Create NFTokenMetadataEnumerable which is just a copy of enumerable at this point.

---
+++ contracts/tokens/NFTokenMetadataEnumerable.sol
@@ -0,0 +1,536 @@
+pragma solidity ^0.4.24;
+
+import "./ERC721.sol";
+import "./ERC721Enumerable.sol";
+import "./ERC721TokenReceiver.sol";
+import "@0xcert/ethereum-utils/contracts/math/SafeMath.sol";
+import "@0xcert/ethereum-utils/contracts/utils/SupportsInterface.sol";
+import "@0xcert/ethereum-utils/contracts/utils/AddressUtils.sol";
+
+/**
+ * @dev Optional enumeration implementation for ERC-721 non-fungible token standard.
+ */
+contract NFTokenMetadataEnumerable is
+  ERC721,
+  ERC721Enumerable,
+  SupportsInterface
+{
+  using SafeMath for uint256;
+  using AddressUtils for address;
+
+  /**
+   * @dev Array of all NFT IDs.
+   */
+  uint256[] internal tokens;
+
+  /**
+   * @dev Mapping from token ID its index in global tokens array.
+   */
+  mapping(uint256 => uint256) internal idToIndex;
+
+  /**
+   * @dev Mapping from owner to list of owned NFT IDs.
+   */
+  mapping(address => uint256[]) internal ownerToIds;
+
+  /**
+   * @dev Mapping from NFT ID to its index in the owner tokens list.
+   */
+  mapping(uint256 => uint256) internal idToOwnerIndex;
+
+  /**
+   * @dev A mapping from NFT ID to the address that owns it.
+   */
+  mapping (uint256 => address) internal idToOwner;
+
+  /**
+   * @dev Mapping from NFT ID to approved address.
+   */
+  mapping (uint256 => address) internal idToApprovals;
+
+  /**
+   * @dev Mapping from owner address to mapping of operator addresses.
+   */
+  mapping (address => mapping (address => bool)) internal ownerToOperators;
+  
+
+  /**
+   * @dev Magic value of a smart contract that can recieve NFT.
+   * Equal to: bytes4(keccak256("onERC721Received(address,address,uint256,bytes)")).
+   */
+  bytes4 constant MAGIC_ON_ERC721_RECEIVED = 0x150b7a02;
+
+  /**
+   * @dev Emits when ownership of any NFT changes by any mechanism. This event emits when NFTs are
+   * created (`from` == 0) and destroyed (`to` == 0). Exception: during contract creation, any
+   * number of NFTs may be created and assigned without emitting Transfer. At the time of any
+   * transfer, the approved address for that NFT (if any) is reset to none.
+   * @param _from Sender of NFT (if address is zero address it indicates token creation).
+   * @param _to Receiver of NFT (if address is zero address it indicates token destruction).
+   * @param _tokenId The NFT that got transfered.
+   */
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 indexed _tokenId
+  );
+
+  /**
+   * @dev This emits when the approved address for an NFT is changed or reaffirmed. The zero
+   * address indicates there is no approved address. When a Transfer event emits, this also
+   * indicates that the approved address for that NFT (if any) is reset to none.
+   * @param _owner Owner of NFT.
+   * @param _approved Address that we are approving.
+   * @param _tokenId NFT which we are approving.
+   */
+  event Approval(
+    address indexed _owner,
+    address indexed _approved,
+    uint256 indexed _tokenId
+  );
+
+  /**
+   * @dev This emits when an operator is enabled or disabled for an owner. The operator can manage
+   * all NFTs of the owner.
+   * @param _owner Owner of NFT.
+   * @param _operator Address to which we are setting operator rights.
+   * @param _approved Status of operator rights(true if operator rights are given and false if
+   * revoked).
+   */
+  event ApprovalForAll(
+    address indexed _owner,
+    address indexed _operator,
+    bool _approved
+  );
+
+  /**
+   * @dev Contract constructor.
+   */
+  constructor()
+    public
+  {
+    supportedInterfaces[0x80ac58cd] = true; // ERC721
+    supportedInterfaces[0x780e9d63] = true; // ERC721Enumerable
+  }
+
+  /**
+   * @dev Returns the number of NFTs owned by `_owner`. NFTs assigned to the zero address are
+   * considered invalid, and this function throws for queries about the zero address.
+   * @param _owner Address for whom to query the balance.
+   */
+  function balanceOf(
+    address _owner
+  )
+    external
+    view
+    returns (uint256)
+  {
+    require(_owner != address(0));
+    return ownerToIds[_owner].length;
+  }
+
+  /**
+   * @dev Returns the address of the owner of the NFT. NFTs assigned to zero address are considered
+   * invalid, and queries about them do throw.
+   * @param _tokenId The identifier for an NFT.
+   */
+  function ownerOf(
+    uint256 _tokenId
+  )
+    external
+    view
+    returns (address _owner)
+  {
+    _owner = idToOwner[_tokenId];
+    require(_owner != address(0));
+  }
+
+  /**
+   * @dev Transfers the ownership of an NFT from one address to another address.
+   * @notice Throws unless `msg.sender` is the current owner, an authorized operator, or the
+   * approved address for this NFT. Throws if `_from` is not the current owner. Throws if `_to` is
+   * the zero address. Throws if `_tokenId` is not a valid NFT. When transfer is complete, this
+   * function checks if `_to` is a smart contract (code size > 0). If so, it calls 
+   * `onERC721Received` on `_to` and throws if the return value is not 
+   * `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`.
+   * @param _from The current owner of the NFT.
+   * @param _to The new owner.
+   * @param _tokenId The NFT to transfer.
+   * @param _data Additional data with no specified format, sent in call to `_to`.
+   */
+  function safeTransferFrom(
+    address _from,
+    address _to,
+    uint256 _tokenId,
+    bytes _data
+  )
+    external
+  {
+    // valid NFT
+    require(_from != address(0));
+    require(idToOwner[_tokenId] == _from);
+    require(_to != address(0));
+
+    // can transfer
+    require(
+      _from == msg.sender
+      || idToApprovals[_tokenId] == msg.sender
+      || ownerToOperators[_from][msg.sender]
+    );
+
+    if (_to.isContract()) {
+      require(
+        ERC721TokenReceiver(_to)
+          .onERC721Received(msg.sender, _from, _tokenId, _data) == MAGIC_ON_ERC721_RECEIVED
+      );
+    }
+
+    // clear approval
+    if(idToApprovals[_tokenId] != 0)
+    {
+      delete idToApprovals[_tokenId];
+    }
+
+    // remove NFT
+    assert(ownerToIds[_from].length > 0);
+
+    uint256 tokenToRemoveIndex = idToOwnerIndex[_tokenId];
+    uint256 lastTokenIndex = ownerToIds[_from].length - 1;
+
+    if(lastTokenIndex != tokenToRemoveIndex)
+    {
+      uint256 lastToken = ownerToIds[_from][lastTokenIndex];
+      ownerToIds[_from][tokenToRemoveIndex] = lastToken;
+      idToOwnerIndex[lastToken] = tokenToRemoveIndex;
+    }
+
+    ownerToIds[_from].length--;
+
+    // add NFT
+    idToOwner[_tokenId] = _to;
+    uint256 length = ownerToIds[_to].push(_tokenId);
+    idToOwnerIndex[_tokenId] = length - 1;
+
+    emit Transfer(_from, _to, _tokenId);
+  }
+
+  /**
+   * @dev Transfers the ownership of an NFT from one address to another address.
+   * @notice This works identically to the other function with an extra data parameter, except this
+   * function just sets data to "".
+   * @param _from The current owner of the NFT.
+   * @param _to The new owner.
+   * @param _tokenId The NFT to transfer.
+   */
+  function safeTransferFrom(
+    address _from,
+    address _to,
+    uint256 _tokenId
+  )
+    external
+  {
+    // valid NFT
+    require(_from != address(0));
+    require(idToOwner[_tokenId] == _from);
+    require(_to != address(0));
+
+    // can transfer
+    require(
+      _from == msg.sender
+      || idToApprovals[_tokenId] == msg.sender
+      || ownerToOperators[_from][msg.sender]
+    );
+
+    if (_to.isContract()) {
+      require(
+        ERC721TokenReceiver(_to)
+          .onERC721Received(msg.sender, _from, _tokenId, "") == MAGIC_ON_ERC721_RECEIVED
+      );
+    }
+
+    // clear approval
+    if(idToApprovals[_tokenId] != 0)
+    {
+      delete idToApprovals[_tokenId];
+    }
+
+    // remove NFT
+    assert(ownerToIds[_from].length > 0);
+
+    uint256 tokenToRemoveIndex = idToOwnerIndex[_tokenId];
+    uint256 lastTokenIndex = ownerToIds[_from].length - 1;
+
+    if(lastTokenIndex != tokenToRemoveIndex)
+    {
+      uint256 lastToken = ownerToIds[_from][lastTokenIndex];
+      ownerToIds[_from][tokenToRemoveIndex] = lastToken;
+      idToOwnerIndex[lastToken] = tokenToRemoveIndex;
+    }
+
+    ownerToIds[_from].length--;
+
+    // add NFT
+    idToOwner[_tokenId] = _to;
+    uint256 length = ownerToIds[_to].push(_tokenId);
+    idToOwnerIndex[_tokenId] = length - 1;
+
+    emit Transfer(_from, _to, _tokenId);
+  }
+
+  /**
+   * @dev Throws unless `msg.sender` is the current owner, an authorized operator, or the approved
+   * address for this NFT. Throws if `_from` is not the current owner. Throws if `_to` is the zero
+   * address. Throws if `_tokenId` is not a valid NFT.
+   * @notice The caller is responsible to confirm that `_to` is capable of receiving NFTs or else
+   * they maybe be permanently lost.
+   * @param _from The current owner of the NFT.
+   * @param _to The new owner.
+   * @param _tokenId The NFT to transfer.
+   */
+  function transferFrom(
+    address _from,
+    address _to,
+    uint256 _tokenId
+  )
+    external
+  {
+    // valid NFT
+    require(_from != address(0));
+    require(idToOwner[_tokenId] == _from);
+    require(_to != address(0));
+
+    // can transfer
+    require(
+      _from == msg.sender
+      || idToApprovals[_tokenId] == msg.sender
+      || ownerToOperators[_from][msg.sender]
+    );
+
+    // clear approval
+    if(idToApprovals[_tokenId] != 0)
+    {
+      delete idToApprovals[_tokenId];
+    }
+
+    // remove NFT
+    assert(ownerToIds[_from].length > 0);
+
+    uint256 tokenToRemoveIndex = idToOwnerIndex[_tokenId];
+    uint256 lastTokenIndex = ownerToIds[_from].length - 1;
+
+    if(lastTokenIndex != tokenToRemoveIndex)
+    {
+      uint256 lastToken = ownerToIds[_from][lastTokenIndex];
+      ownerToIds[_from][tokenToRemoveIndex] = lastToken;
+      idToOwnerIndex[lastToken] = tokenToRemoveIndex;
+    }
+
+    ownerToIds[_from].length--;
+
+    // add NFT
+    idToOwner[_tokenId] = _to;
+    uint256 length = ownerToIds[_to].push(_tokenId);
+    idToOwnerIndex[_tokenId] = length - 1;
+
+    emit Transfer(_from, _to, _tokenId);
+  }
+
+  /**
+   * @dev Set or reaffirm the approved address for an NFT.
+   * @notice The zero address indicates there is no approved address. Throws unless `msg.sender` is
+   * the current NFT owner, or an authorized operator of the current owner.
+   * @param _approved Address to be approved for the given NFT ID.
+   * @param _tokenId ID of the token to be approved.
+   */
+  function approve(
+    address _approved,
+    uint256 _tokenId
+  )
+    external
+  {
+    // can operate
+    address tokenOwner = idToOwner[_tokenId];
+    require(tokenOwner == msg.sender || ownerToOperators[tokenOwner][msg.sender]);
+
+    idToApprovals[_tokenId] = _approved;
+    emit Approval(tokenOwner, _approved, _tokenId);
+  }
+
+  /**
+   * @dev Enables or disables approval for a third party ("operator") to manage all of
+   * `msg.sender`'s assets. It also emits the ApprovalForAll event.
+   * @notice This works even if sender doesn't own any tokens at the time.
+   * @param _operator Address to add to the set of authorized operators.
+   * @param _approved True if the operators is approved, false to revoke approval.
+   */
+  function setApprovalForAll(
+    address _operator,
+    bool _approved
+  )
+    external
+  {
+    ownerToOperators[msg.sender][_operator] = _approved;
+    emit ApprovalForAll(msg.sender, _operator, _approved);
+  }
+
+  /**
+   * @dev Get the approved address for a single NFT.
+   * @notice Throws if `_tokenId` is not a valid NFT.
+   * @param _tokenId ID of the NFT to query the approval of.
+   */
+  function getApproved(
+    uint256 _tokenId
+  )
+    external
+    view
+    returns (address)
+  {
+    require(idToOwner[_tokenId] != address(0));
+    return idToApprovals[_tokenId];
+  }
+
+  /**
+   * @dev Checks if `_operator` is an approved operator for `_owner`.
+   * @param _owner The address that owns the NFTs.
+   * @param _operator The address that acts on behalf of the owner.
+   */
+  function isApprovedForAll(
+    address _owner,
+    address _operator
+  )
+    external
+    view
+    returns (bool)
+  {
+    return ownerToOperators[_owner][_operator];
+  }
+
+  /**
+   * @dev Mints a new NFT.
+   * @notice This is a private function which should be called from user-implemented external
+   * mint function. Its purpose is to show and properly initialize data structures when using this
+   * implementation.
+   * @param _to The address that will own the minted NFT.
+   * @param _tokenId of the NFT to be minted by the msg.sender.
+   */
+  function _mint(
+    address _to,
+    uint256 _tokenId
+  )
+    internal
+  {
+    require(_to != address(0));
+    require(idToOwner[_tokenId] == address(0));
+
+    // add NFT
+    idToOwner[_tokenId] = _to;
+
+    uint256 length = ownerToIds[_to].push(_tokenId);
+    idToOwnerIndex[_tokenId] = length - 1;
+
+    // add to tokens array
+    length = tokens.push(_tokenId);
+    idToIndex[_tokenId] = length - 1;
+
+    emit Transfer(address(0), _to, _tokenId);
+  }
+
+  /**
+   * @dev Burns a NFT.
+   * @notice This is a private function which should be called from user-implemented external
+   * burn function. Its purpose is to show and properly initialize data structures when using this
+   * implementation.
+   * @param _tokenId ID of the NFT to be burned.
+   */
+  function _burn(
+    uint256 _tokenId
+  )
+    internal
+  {
+    // valid NFT
+    address owner = idToOwner[_tokenId];
+    require(owner != address(0));
+
+    // clear approval
+    if(idToApprovals[_tokenId] != 0)
+    {
+      delete idToApprovals[_tokenId];
+    }
+
+    // remove NFT
+    assert(ownerToIds[owner].length > 0);
+
+    uint256 tokenToRemoveIndex = idToOwnerIndex[_tokenId];
+    uint256 lastTokenIndex = ownerToIds[owner].length - 1;
+
+    if(lastTokenIndex != tokenToRemoveIndex)
+    {
+      uint256 lastToken = ownerToIds[owner][lastTokenIndex];
+      ownerToIds[owner][tokenToRemoveIndex] = lastToken;
+      idToOwnerIndex[lastToken] = tokenToRemoveIndex;
+    }
+
+    delete idToOwner[_tokenId];
+    delete idToOwnerIndex[_tokenId];
+    ownerToIds[owner].length--;
+
+    // remove from tokens array
+    assert(tokens.length > 0);
+
+    uint256 tokenIndex = idToIndex[_tokenId];
+    lastTokenIndex = tokens.length - 1;
+    lastToken = tokens[lastTokenIndex];
+
+    tokens[tokenIndex] = lastToken;
+
+    tokens.length--;
+    // Consider adding a conditional check for the last token in order to save GAS.
+    idToIndex[lastToken] = tokenIndex;
+    idToIndex[_tokenId] = 0;
+
+    emit Transfer(owner, address(0), _tokenId);
+  }
+
+  /**
+   * @dev Returns the count of all existing NFTokens.
+   */
+  function totalSupply()
+    external
+    view
+    returns (uint256)
+  {
+    return tokens.length;
+  }
+
+  /**
+   * @dev Returns NFT ID by its index.
+   * @param _index A counter less than `totalSupply()`.
+   */
+  function tokenByIndex(
+    uint256 _index
+  )
+    external
+    view
+    returns (uint256)
+  {
+    require(_index < tokens.length);
+    return tokens[_index];
+  }
+
+  /**
+   * @dev returns the n-th NFT ID from a list of owner's tokens.
+   * @param _owner Token owner's address.
+   * @param _index Index number representing n-th token in owner's list of tokens.
+   */
+  function tokenOfOwnerByIndex(
+    address _owner,
+    uint256 _index
+  )
+    external
+    view
+    returns (uint256)
+  {
+    require(_index < ownerToIds[_owner].length);
+    return ownerToIds[_owner][_index];
+  }
+}
