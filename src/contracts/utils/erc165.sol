9d4dcde38206559ca603fae8449ecf61c41757d2
Tadej Vengust <vengust.tadej@gmail.com>
2018-12-05 16:28:54 +0100

    Add lower case erc721 and erc165.

---
+++ src/contracts/utils/erc165.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.24;
+
+/**
+ * @dev A standard for detecting smart contract interfaces. 
+ * See: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-165.md.
+ */
+interface ERC165 {
+
+  /**
+   * @dev Checks if the smart contract includes a specific interface.
+   * @notice This function uses less than 30,000 gas.
+   * @param _interfaceID The interface identifier, as specified in ERC-165.
+   */
+  function supportsInterface(
+    bytes4 _interfaceID
+  )
+    external
+    view
+    returns (bool);
+}
