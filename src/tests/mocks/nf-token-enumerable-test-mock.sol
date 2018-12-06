812772edbd8f566767eb757f4d9b9cf467ebfca1
Tadej Vengust <vengust.tadej@gmail.com>
2018-12-06 13:41:05 +0100

    Fix nf token test mocks.

--- src/tests/mocks/nf-token-enumerable-test-mock.sol
+++ src/tests/mocks/nf-token-enumerable-test-mock.sol
@@ -36,21 +36,13 @@ contract NFTokenEnumerableTestMock is
     return idToOwnerIndex[_tokenId];
   }
 
-  function removeNFTokenWrapper(
-    address _from,
-    uint256 _tokenId
-  )
-   external
-  {
-    super.removeNFToken(_from, _tokenId);
-  }
-
-  function addNFTokenWrapper(
-    address _to,
+  function idToIndexWrapper(
     uint256 _tokenId
   )
     external
+    view
+    returns (uint256)
   {
-    super.addNFToken(_to, _tokenId);
+    return idToIndex[_tokenId];
   }
 }
