494048b920c44f24e73c69482f09fff59bc7c9b9
Tadej Vengust <vengust.tadej@gmail.com>
2019-01-22 10:23:24 +0100

    Typo fix.

--- src/contracts/tokens/nf-token-metadata.sol
+++ src/contracts/tokens/nf-token-metadata.sol
@@ -99,7 +99,7 @@ contract NFTokenMetadata is
 
   /**
    * @dev Set a distinct URI (RFC 3986) for a given NFT ID.
-   * @notice This is a internal function which should be called from user-implemented external
+   * @notice This is an internal function which should be called from user-implemented external
    * function. Its purpose is to show and properly initialize data structures when using this
    * implementation.
    * @param _tokenId Id for which we want uri.
